c07a521c01052c61f41b7eeb65656319facf4e3c
Michael Schiumo <mschiumo18@gmail.com>
2019-04-05 17:42:57 -0400

    built out users controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -20,7 +20,7 @@ class ExercisesController < ApplicationController
   post '/exercises' do
     if logged_in?
       if params["exercise"]["name"]= "" || params["exercise"]["muscle_group"]= ""
-        flash[:message] "Please fill in all fields."
+        flash[:message] = "Please fill in all fields."
         redirect to '/exercises/new'
       else
           @user = current_user
@@ -45,17 +45,45 @@ class ExercisesController < ApplicationController
 
   get '/exercises/:id/edit' do
     if logged_in?
-      @exercise = @exercise.find_by_id[params(:id)]
+      @exercise = Exercise.find_by_id[params(:id)]
       if @exercise && @exercise.user == current_user
-        redirect to '/exercises/edit'
+        erb :'/exercises/edit'
+      else
+        redirect to '/exercises'
       end
+    else
+      redirect to '/login'
     end
   end
 
   patch '/exercises/:id' do
     if logged_in?
-      
+      if params["exercise"]["name"]= "" || params["exercise"]["muscle_group"]= ""
+        redirect to "/exercises/:id/edit"
+      else
+        @exercise = @exercise.find_by_id(id: params[:id])
+      if @exercise && @exercise.user = current_user
+        if @exercise.update(name: params["exercise"]["name"], muscle_group: params["exercise"]["muscle_group"])
+          flash[:message] = "Nice job! You have successfully updated this exercise."
+          redirect to '/exercises'
+        else
+          redirect to '/exercises'
+        end
+      end
+    end
   end
 
-
+  delete '/exercises/:id/delete' do
+    if logged_in?
+      @exercise = @exercise.find_by_id(id: params[:id])
+      if @exercise && @exercise.user == current_user
+        @exercise.delete
+        flash[:message] = "This exercise has been successfully deleted."
+      end
+        redirect to '/exercises'
+    else
+        redirect to '/login'
+    end
+  end
+ end
 end
